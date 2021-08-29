17c934eb8f3ba7b15fa54e0919bc7956138f3d57
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-08-28 18:44:49 -0700

    Added components into App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { ChakraProvider, theme, VStack } from '@chakra-ui/react';
-import Header from './components/Header/Header';
+import { ChakraProvider, theme, useDisclosure, VStack } from '@chakra-ui/react';
+import { Header } from './components/Header/Header';
+import { Inventory } from './components/Inventory/Inventory';
+import { CartProvider } from './context/CartProvider';
+import { Cart } from './components/Cart/Cart';
+import INVENTORY from './data/inventory';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <VStack>
-      <Header></Header>
-    </VStack>
-  </ChakraProvider>
-);
+export const App = () => {
+  const {
+    isOpen: cartIsOpen,
+    onOpen: onCartOpen,
+    onClose: onCartClose,
+  } = useDisclosure();
+
+  return (
+    <ChakraProvider theme={theme}>
+      <CartProvider>
+        <Cart cartIsOpen={cartIsOpen} onCartClose={onCartClose}></Cart>
+        <VStack>
+          <Header onCartOpen={onCartOpen}></Header>
+          <Inventory invetoryItems={INVENTORY} />
+        </VStack>
+      </CartProvider>
+    </ChakraProvider>
+  );
+};
