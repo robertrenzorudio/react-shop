739f4d6063714a3f1a9c0a24d376deb304a6a0a3
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-09-01 20:16:55 -0700

    Implemented GET '/inventory/'.

--- server/src/routes/inventory.ts
+++ server/src/routes/inventory.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { prisma } from '../db/prisma';
 
 const inventoryRouter = express.Router();
 
 inventoryRouter.get('/', async (_, res) => {
-  res.json({ inventory: 'route' });
+  const inventory = await prisma.inventory.findMany();
+  res.json({ inventory });
 });
 
 export default inventoryRouter;
