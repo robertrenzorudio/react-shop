db5abef6358f7d764e988e5558bd47dd91f66c8d
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-09-01 18:35:29 -0700

    Init server repo.

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const main = async () => {
+  const app = express();
+
+  app.use('/assets', express.static('public/inventory'));
+
+  app.get('/', (_, res) => {
+    res.send('hello world');
+  });
+
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`Server ready at: http://localhost:${port}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err.message);
+});
