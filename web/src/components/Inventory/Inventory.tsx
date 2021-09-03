5a39391a0cf3669ffab31b47a8467f8bb5e642c9
Robert Renzo Rudio <68797863+robertrenzorudio@users.noreply.github.com>
2021-09-02 19:51:46 -0700

    Inventory fetching from server. (#6)
    * Added spinner when loading images.
    
    * Changed Box to Flex to ceter align spinner.
    
    * Added a custom useFetch hook and used it in Inventory component.
    
    * setIsFetching to false on fetch error.
    
    * Added fetch error handling.

--- web/src/components/Inventory/Inventory.tsx
+++ web/src/components/Inventory/Inventory.tsx
@@ -1,26 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useFetch } from '../../hooks/useFetch';
 import { CartContext } from '../../context/cart-context';
-import { Flex, Spinner } from '@chakra-ui/react';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
 import { InventoryItem } from './InventoryItem';
 import { InventoryItemType } from '../../interface/InventoryItemType';
+import { BiError } from 'react-icons/bi';
 
 interface InventoryProps {}
 
 export const Inventory: React.FC<InventoryProps> = () => {
   const cartCtx = useContext(CartContext);
-  const [inventory, setInventory] = useState<InventoryItemType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const {
+    data: inventory,
+    isFetching: isLoading,
+    error,
+    sendRequest,
+  } = useFetch<InventoryItemType[]>([]);
 
   useEffect(() => {
-    const fetchInventory = async () => {
-      const res = await fetch('https://api-react-shop.herokuapp.com/inventory');
-      const data = (await res.json()) as {
-        inventory: (InventoryItemType & {
-          image: string;
-        })[];
-      };
+    type FetchType = {
+      inventory: (InventoryItemType & {
+        image: string;
+      })[];
+    };
 
-      const transformedData: InventoryItemType[] = data.inventory.map(
+    const dataTransformFn = (data: FetchType) => {
+      return data.inventory.map(
         ({ id, manufacturer, name, price, formattedPrice, image }) => {
           return {
             id,
@@ -31,13 +36,42 @@ export const Inventory: React.FC<InventoryProps> = () => {
             imgSrc: image,
           };
         }
-      );
-      setInventory(transformedData);
-      setIsLoading(false);
+      ) as InventoryItemType[];
     };
 
-    fetchInventory();
-  }, []);
+    sendRequest(
+      { input: 'https://api-react-shop..com/inventory' },
+      dataTransformFn
+    );
+  }, [sendRequest]);
+
+  if (error) {
+    return (
+      <Flex direction="column" alignItems="center" p={2}>
+        <BiError size={60} color="#718096" />
+        <Text
+          color="#718096"
+          fontSize="4xl"
+          fontWeight="extrabold"
+          align="center"
+        >
+          An error has occured!
+        </Text>
+        <Text
+          color="#718096"
+          fontSize="4xl"
+          fontWeight="extrabold"
+          align="center"
+        >
+          Please try again later.
+        </Text>
+      </Flex>
+    );
+  }
+
+  if (isLoading) {
+    return <Spinner speed="1s" size="xl" mt={100} />;
+  }
 
   const addToCartHandler = (item: InventoryItemType) => {
     cartCtx.addItem(item!);
@@ -51,17 +85,7 @@ export const Inventory: React.FC<InventoryProps> = () => {
 
   return (
     <Flex direction="row" width="80%" justifyContent="space-around" wrap="wrap">
-      {!isLoading ? (
-        inventoryJSX
-      ) : (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      )}
+      {inventoryJSX}
     </Flex>
   );
 };
--- web/src/components/Inventory/InventoryImage.tsx
+++ web/src/components/Inventory/InventoryImage.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Image } from '@chakra-ui/react';
+import { Image, Spinner, Flex } from '@chakra-ui/react';
 
 interface InventoryImageProps {
   src: string;
 }
 
 export const InventoryImage: React.FC<InventoryImageProps> = ({ src }) => {
+  const height = 230;
+  const width = 282;
+
   return (
     <Image
       rounded="lg"
-      height={230}
-      width={282}
+      height={height}
+      width={width}
       objectFit="contain"
       src={src}
+      fallback={
+        <Flex
+          height={height}
+          width={width}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Spinner size="xl" speed="1.0s" />
+        </Flex>
+      }
     />
   );
 };
