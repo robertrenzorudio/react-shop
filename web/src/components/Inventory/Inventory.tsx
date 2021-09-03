ba52d5322315664410cd81eecd7bc607113e0b73
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-09-02 19:46:10 -0700

    Added fetch error handling.

--- web/src/components/Inventory/Inventory.tsx
+++ web/src/components/Inventory/Inventory.tsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import { useFetch } from '../../hooks/useFetch';
 import { CartContext } from '../../context/cart-context';
-import { Flex, Spinner } from '@chakra-ui/react';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
 import { InventoryItem } from './InventoryItem';
 import { InventoryItemType } from '../../interface/InventoryItemType';
+import { BiError } from 'react-icons/bi';
 
 interface InventoryProps {}
 
@@ -12,6 +13,7 @@ export const Inventory: React.FC<InventoryProps> = () => {
   const {
     data: inventory,
     isFetching: isLoading,
+    error,
     sendRequest,
   } = useFetch<InventoryItemType[]>([]);
 
@@ -38,10 +40,34 @@ export const Inventory: React.FC<InventoryProps> = () => {
     };
 
     sendRequest(
-      { input: 'https://api-react-shop.herokuapp.com/inventory' },
+      { input: 'https://api-react-shop..com/inventory' },
       dataTransformFn
     );
   }, [sendRequest]);
+
+  if (error) {
+    return (
+      <Flex direction="column" alignItems="center" p={2}>
+        <BiError size={60} color="#718096" />
+        <Text
+          color="#718096"
+          fontSize="4xl"
+          fontWeight="extrabold"
+          align="center"
+        >
+          An error has occured!
+        </Text>
+        <Text
+          color="#718096"
+          fontSize="4xl"
+          fontWeight="extrabold"
+          align="center"
+        >
+          Please try again later.
+        </Text>
+      </Flex>
+    );
+  }
 
   if (isLoading) {
     return <Spinner speed="1s" size="xl" mt={100} />;
