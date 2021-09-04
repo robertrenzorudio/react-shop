e44a15cc807b6d3d7819e9dafc38d76475d51ef6
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-09-03 22:29:47 -0700

    Moving step controller inside the Step component's children.

--- web/src/components/Checkout/Checkout.tsx
+++ web/src/components/Checkout/Checkout.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Step, Steps, useSteps } from 'chakra-ui-steps';
-import { Flex, Button, HStack } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { Shipping } from './Shipping';
 
 const content2 = <Flex py={4}>hello world</Flex>;
 const content3 = <Flex py={4}>lorem ispum</Flex>;
 
 const steps = [
-  { label: 'Shipping', content: <Shipping /> },
   { label: 'Payment', content: content2 },
   { label: 'Review', content: content3 },
 ];
@@ -17,6 +16,14 @@ export const Checkout = () => {
     initialStep: 0,
   });
 
+  const prevHandler = () => {
+    prevStep();
+  };
+
+  const nextHandler = () => {
+    nextStep();
+  };
+
   return (
     <Flex
       width={{ sm: '100%', md: '80%', lg: '70%', xl: '60%' }}
@@ -24,32 +31,15 @@ export const Checkout = () => {
       p={4}
     >
       <Steps activeStep={activeStep} colorScheme="teal">
+        <Step label={'Shipping'} key={'Shipping'}>
+          {<Shipping onNext={nextHandler} />}
+        </Step>
         {steps.map(({ label, content }) => (
           <Step label={label} key={label}>
             {content}
           </Step>
         ))}
       </Steps>
-
-      <HStack alignSelf="flex-end" mt={4} mr={4}>
-        <Button
-          onClick={() => {
-            prevStep();
-          }}
-          disabled={activeStep === 0}
-        >
-          Back
-        </Button>
-        <Button
-          onClick={() => {
-            nextStep();
-          }}
-          colorScheme={activeStep === 2 ? 'teal' : 'gray'}
-          isDisabled={activeStep === 2}
-        >
-          {activeStep !== 2 ? 'Next' : 'Place your order'}
-        </Button>
-      </HStack>
     </Flex>
   );
 };
--- web/src/components/Checkout/Shipping.tsx
+++ web/src/components/Checkout/Shipping.tsx
@@ -3,99 +3,106 @@ import { Formik, Form } from 'formik';
 import { MyTextField } from '../Form/MyTextField';
 import { VStack, Stack } from '@chakra-ui/layout';
 import { useMediaQuery } from '@chakra-ui/media-query';
+import { Button } from '@chakra-ui/button';
 
-interface ShippingProps {}
+interface ShippingProps {
+  onNext: () => void;
+}
 
-export const Shipping: React.FC<ShippingProps> = (prop) => {
+export const Shipping: React.FC<ShippingProps> = ({ onNext }) => {
   const [isSmall] = useMediaQuery('(max-width: 425px)');
 
   return (
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-        addressLine1: '',
-        addressLine2: '',
-        city: '',
-        area: '',
-        zipCode: '',
-        country: '',
-      }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {() => (
-        <Form>
-          <VStack spacing={4} mt={isSmall ? 0 : 4} p={1}>
-            <Stack
-              direction={isSmall ? 'column' : 'row'}
-              w="100%"
-              spacing={isSmall ? 4 : 10}
-            >
+    <Stack>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          addressLine1: '',
+          addressLine2: '',
+          city: '',
+          area: '',
+          zipCode: '',
+          country: '',
+        }}
+        onSubmit={(values) => {}}
+      >
+        {() => (
+          <Form>
+            <VStack spacing={4} mt={isSmall ? 0 : 4} p={1}>
+              <Stack
+                direction={isSmall ? 'column' : 'row'}
+                w="100%"
+                spacing={isSmall ? 4 : 10}
+              >
+                <MyTextField
+                  name="firstName"
+                  label="First Name"
+                  isRequired={true}
+                />
+
+                <MyTextField
+                  name="lastName"
+                  label="Last Name"
+                  isRequired={true}
+                />
+              </Stack>
+
               <MyTextField
-                name="firstName"
-                label="First Name"
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="your-name@examle.xyz"
                 isRequired={true}
               />
 
               <MyTextField
-                name="lastName"
-                label="Last Name"
+                name="addressLine1"
+                label="Address line 1"
                 isRequired={true}
               />
-            </Stack>
-
-            <MyTextField
-              type="email"
-              name="email"
-              label="Email"
-              placeholder="your-name@examle.xyz"
-              isRequired={true}
-            />
-
-            <MyTextField
-              name="addressLine1"
-              label="Address line 1"
-              isRequired={true}
-            />
-
-            <MyTextField
-              name="addressLine2"
-              label="Address line 2"
-              isRequired={false}
-            />
-            <Stack
-              direction={isSmall ? 'column' : 'row'}
-              w="100%"
-              spacing={isSmall ? 4 : 10}
-            >
-              <MyTextField name="city" label="City" isRequired={true} />
 
               <MyTextField
-                name="area"
-                label="State/Province/Region"
-                isRequired={true}
+                name="addressLine2"
+                label="Address line 2"
+                isRequired={false}
               />
-            </Stack>
+              <Stack
+                direction={isSmall ? 'column' : 'row'}
+                w="100%"
+                spacing={isSmall ? 4 : 10}
+              >
+                <MyTextField name="city" label="City" isRequired={true} />
 
-            <Stack
-              direction={isSmall ? 'column' : 'row'}
-              w="100%"
-              spacing={isSmall ? 4 : 10}
-            >
-              <MyTextField
-                name="zipCode"
-                label="Zip / Postal Code"
-                isRequired={true}
-              />
+                <MyTextField
+                  name="area"
+                  label="State/Province/Region"
+                  isRequired={true}
+                />
+              </Stack>
 
-              <MyTextField name="country" label="Country" isRequired={true} />
-            </Stack>
-          </VStack>
-        </Form>
-      )}
-    </Formik>
+              <Stack
+                direction={isSmall ? 'column' : 'row'}
+                w="100%"
+                spacing={isSmall ? 4 : 10}
+              >
+                <MyTextField
+                  name="zipCode"
+                  label="Zip / Postal Code"
+                  isRequired={true}
+                />
+
+                <MyTextField name="country" label="Country" isRequired={true} />
+              </Stack>
+            </VStack>
+          </Form>
+        )}
+      </Formik>
+
+      <Button type="submit" alignSelf="flex-end" onClick={onNext} mt={4}>
+        Next
+      </Button>
+    </Stack>
   );
 };
